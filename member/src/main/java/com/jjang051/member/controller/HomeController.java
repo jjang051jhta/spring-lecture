50e8d247ba35895e9ed0f0e3b92157f536ff528e
jjang051 <jjang051jhta@gmail.com>
2025-10-13 11:34:55 +0900

    init

---
+++ member/src/main/java/com/jjang051/member/controller/HomeController.java
@@ -0,0 +1,48 @@
+package com.jjang051.member.controller;
+
+import com.jjang051.member.dto.JoinDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+
+//등록이 된다.
+//  HomeController.java  ===> HomeController.class
+//  new HomeController(); 자바 생성자가 있어야 실행 가능
+//  IoC Inversion of Control  DI
+@Controller
+public class HomeController {
+
+    @GetMapping({"/", "/main", "/index", "/home"})
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/request")
+    public String request(@RequestParam(value = "userID",required = true,defaultValue = "noname") String userID,
+                          @RequestParam(value = "userPW",required = true,defaultValue = "1234") String userPw,
+                          @RequestParam(value="userName",required = true,defaultValue = "홍길동") String userName,
+                          @RequestParam(value="userAge",required = false,defaultValue = "7") int userAge
+                          ) {
+        //userName은 필수 요소
+        //userAge int 필수 요소는 아니다.
+        System.out.println(userID + "====" + userPw+"===="+userName+"===="+userAge);
+        return "request";
+    }
+    @GetMapping("/request02")
+    public String request02(@ModelAttribute JoinDto joinDto) {
+        System.out.println(joinDto);
+        return "request02";
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
