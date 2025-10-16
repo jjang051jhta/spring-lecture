7a5f2d700cb255f9190a117b6d89af728b396687
jjang051 <jjang051jhta@gmail.com>
2025-10-16 13:33:53 +0900

    validation

--- validation/src/main/java/com/jjang051/validation/dto/MemberDto.java
+++ validation/src/main/java/com/jjang051/validation/dto/MemberDto.java
@@ -1,9 +1,6 @@
 package com.jjang051.validation.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Getter
@@ -21,6 +18,8 @@ public class MemberDto {
 
     @NotBlank(message = "비밀번호는 필수입력사항입니다.")
     @Size(min = 2, max = 16, message = "비민번호는 2글자 이상 16자 이하로 압력해주세요.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-={}\\[\\]:;\"'<>,.?/]).+$",
+            message = "비밀번호는 대문자,소문자,특수문자,숫자를 모두 포함하여야 합니다.")
     private String userPW;
 
     @NotBlank(message = "이름은 필수입력사항입니다.")
