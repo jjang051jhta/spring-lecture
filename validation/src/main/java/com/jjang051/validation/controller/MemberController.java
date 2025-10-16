42ee5538d0a979ff26e913ce40f22c5f73afb2d6
jjang051 <jjang051jhta@gmail.com>
2025-10-16 12:17:41 +0900

    validation

--- validation/src/main/java/com/jjang051/validation/controller/MemberController.java
+++ validation/src/main/java/com/jjang051/validation/controller/MemberController.java
@@ -1,15 +1,22 @@
 package com.jjang051.validation.controller;
 
+import com.jjang051.validation.dto.LoginDto;
 import com.jjang051.validation.dto.MemberDto;
+import com.jjang051.validation.repository.MemberRepository;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/member")
 public class MemberController {
+
+    private final MemberRepository memberRepository;
+
     @GetMapping("/add")
     public String add(Model model) {
         model.addAttribute("memberDto",new MemberDto());
@@ -20,7 +27,7 @@ public class MemberController {
                              BindingResult bindingResult, Model model){
         if (bindingResult.hasErrors()){
             System.out.println(memberDto);
-            model.addAttribute("msg",true);
+            //model.addAttribute("msg",true);
             return "member/add";
         }
         System.out.println(memberDto);
@@ -31,4 +38,23 @@ public class MemberController {
     public String detail(@PathVariable("id") Integer id){
         return "member/detail";
     }
+    @GetMapping("/login")
+    public String login(Model model){
+        model.addAttribute("loginDto",new LoginDto());
+        return "member/login";
+    }
+    @PostMapping("/login")
+    public String loginProcess(@Valid @ModelAttribute LoginDto loginDto,
+                               BindingResult bindingResult,
+                               Model model){
+        System.out.println(loginDto.toString());
+        if (bindingResult.hasErrors()){
+            return "member/login";
+        }
+        MemberDto loginMember = memberRepository.findById(loginDto);
+        System.out.println(loginMember.toString());
+        return "member/list";
+    }
+
+
 }
